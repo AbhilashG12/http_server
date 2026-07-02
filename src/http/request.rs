c43715c469f7da1ab0366fa99c5effbab56636c0
AbhilashG12 <abhilashggg15@gmail.com>
2026-07-02 14:36:36 +0530

    Initial commit

---
+++ src/http/request.rs
@@ -0,0 +1,38 @@
+use std::io::{self,Read};
+use std::net::TcpStream;
+
+pub struct HttpRequest{
+    pub raw_data : Vec<u8>
+}
+
+pub fn handle_connection(mut stream : TcpStream) -> io::Result<()> {
+    
+    let mut buffer = [0;512];
+    let mut raw_request = Vec::new();
+    
+    loop {
+        let bytes_read = stream.read(&mut buffer)?;
+        if bytes_read == 0 {
+            break;
+        }
+
+        raw_request.extend_from_slice(&buffer[..bytes_read]);
+
+        if has_reached_end_of_headers(&raw_request){
+            break;
+        }
+    }
+
+    let request = HttpRequest {raw_data : raw_request};
+
+    let parsed_string = String::from_utf8_lossy(&request.raw_data);
+    println!("\n--- [RECEIVED RAW HTTP REQUEST] ---");
+    println!("{}", parsed_string);
+    println!("------------------------------------\n");
+    Ok(())
+
+}
+
+fn has_reached_end_of_headers(bytes:&[u8]) -> bool{
+    bytes.windows(4).any(|window| window == b"\r\n\r\n")
+}
---
+++ src/main.rs
@@ -0,0 +1,31 @@
+use std::io;
+use std::net::{TcpListener};
+pub mod http;
+
+fn main() -> io::Result<()>{
+    let address = String::from("127.0.0.1:7878");
+    let listen = TcpListener::bind(&address);
+    println!("SUCCESS TcpListener is Running on {}",&address);
+    println!("Waiting for connections.......\n");
+
+    for res in listen?.incoming(){
+        match res {
+            Ok(stream) =>{
+                match stream.peer_addr(){
+                    Ok(peer_addr) =>{
+                        println!("[CONNECTED!] New Client established from IP : {} , port : {} ",peer_addr.ip(),peer_addr.port());
+                    }
+                    Err(e)=>{
+                        println!("[WARNING!!] Connected client , but couldnt resolve peer address {}",e);
+                    }
+                }
+            }
+            Err(e)=>{
+                eprintln!("[ERROR] Failed to accept incoming connections : {}",e);
+            }
+        }
+    }
+
+        Ok(())
+
+}
